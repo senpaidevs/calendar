6297d99779d661d1f02dffb1b3111e907adecf29
Néstor Salceda <nestor.salceda@gmail.com>
2015-03-31 18:11:37 +0200

    Order events by date

--- calendar.rb
+++ calendar.rb
@@ -4,7 +4,7 @@ require 'slim'
 require_relative 'event'
 
 get '/' do
-  @events = Event.all(:date.gte => Date.today, :date.lt => Date.today + 31)
+  @events = Event.all(:date.gte => Date.today, :date.lt => Date.today + 31, :order => [:date.asc])
 
   slim :index, locals: {:events => @events}
 end
--- spec/show_city_events_spec.rb
+++ spec/show_city_events_spec.rb
@@ -3,14 +3,17 @@ require_relative '../event'
 describe 'Show events in the city' do
   context 'when querying events in next 30 days' do
     let(:a_title) {'irrelevant title'}
+    let(:tomorrow_event_title) {'tomorrow event title'}
     let(:past_event_title) {'past event title'}
     let(:greater_than_30_days_event_title) {'greater than 30 days event title'}
     let(:today) {Date.today}
+    let(:tomorrow) {Date.today + 1}
     let(:yesterday) {Date.today - 1}
 
     before(:each) do
       DataMapper.auto_migrate!
 
+      Event.create(title: tomorrow_event_title, date: tomorrow)
       Event.create(title: a_title, date: today)
       Event.create(title: past_event_title, date: yesterday)
       Event.create(title: greater_than_30_days_event_title, date: today + 31)
@@ -33,6 +36,13 @@ describe 'Show events in the city' do
 
       it 'contains events within 30 days' do
         expect(last_response.body).to include(a_title)
+      end
+
+      it 'returns new events first' do
+        today_event_order = last_response.body.index(a_title)
+        tomorrow_event_order = last_response.body.index(tomorrow_event_title)
+
+        expect(today_event_order).to be <= tomorrow_event_order
       end
     end
   end
