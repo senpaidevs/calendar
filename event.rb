cd5c8ba9361e6c22cbf083b1cce6ab462f147c2e
Néstor Salceda <nestor.salceda@gmail.com>
2015-04-14 19:10:58 +0200

    Remove datamapper and use activerecord

--- event.rb
+++ event.rb
@@ -1,33 +1,16 @@
-require 'dm-core'
-require 'dm-migrations'
+require 'activerecord'
 require 'geocoder'
 
-DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+class Event < ActiveRecord::Base
+  before_save :geolocalize
 
+  private
+    def geolocalize
+      results = Geocoder.search(address)
 
-class Event
-  include DataMapper::Resource
-  property :id, Serial
-  property :title, String
-  property :address, String
-  property :place, String
-  property :description, Text
-  property :date, DateTime
-  property :url, String
-  property :lat, Float
-  property :long, Float
-
-  before :save, :geolocalize
-
-  def geolocalize 
-
-  	results = Geocoder.search(address)
-  	
-  	if results.size > 0
-	  self.lat = results.first.latitude
-	  self.long = results.first.longitude	  	  	
-  	end
-  end
+      if results.size > 0
+        self.lat = results.first.latitude
+        self.long = results.first.longitude
+      end
+    end
 end
-
-DataMapper.finalize
