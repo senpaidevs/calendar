a27ac4f784002adf5a970216bc54a73e0ec770f2
eckelon <eckelon@gmail.com>
2015-04-14 19:45:31 +0200

    now all fake events are located in Zaragoza by default

--- load_events.rb
+++ load_events.rb
@@ -4,5 +4,5 @@ require_relative 'event'
 DataMapper.auto_migrate!
 
 20.times do
-  Event.create(title: Faker::App.name,url: Faker::Internet.url,address: Faker::Address.street_address,place: Faker::Company.name, description: Faker::Hacker.say_something_smart, date: Faker::Date.forward(60))
+  Event.create(title: Faker::App.name,url: Faker::Internet.url,address:"Zaragoza",place: Faker::Company.name, description: Faker::Hacker.say_something_smart, date: Faker::Date.forward(60))
 end
