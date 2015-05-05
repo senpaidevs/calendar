4592c748c825b1cb3e0f2c96a8803461846785bb
Néstor Salceda <nestor.salceda@gmail.com>
2015-05-05 18:07:58 +0200

    Retab!

--- lib/calendar/event.rb
+++ lib/calendar/event.rb
@@ -5,16 +5,15 @@ class Event < ActiveRecord::Base
   before_save :geolocalize
 
   private
-    def geolocalize
-	  unless self.lat or self.long
-	    		
-	    	
-	      results = Geocoder.search(address)
 
-	      if results.size > 0
-	        self.lat = results.first.latitude
-	        self.long = results.first.longitude
-	      end
-	    end
-	  end
+  def geolocalize
+    unless self.lat or self.long
+      results = Geocoder.search(address)
+
+      if results.size > 0
+        self.lat = results.first.latitude
+        self.long = results.first.longitude
+      end
+    end
+  end
 end
